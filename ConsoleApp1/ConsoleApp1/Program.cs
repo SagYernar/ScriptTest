f0a638c5d9552a54dd8a806d3923dc4766586b54
SagYernar <sagyernar@gmail.com>
2017-10-19 18:00:27 +0600

    eeeeee

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string answers = "asdasdasdasdasdasdasdasdasd";
+
+            List<User> users = new List<User>()
+            {
+                new User()
+                {
+                    Name = "1",
+                    Answers = "asdasdasdasdasdasdasdasdasd"
+                },
+                new User()
+                {
+                    Name = "2",
+                    Answers = "asdasdasdasdasdasdasdasdasd"
+                },
+                new User()
+                {
+                    Name = "3",
+                    Answers = "asdasdasdasdasdasdasdasdasd"
+                },
+                new User()
+                {
+                    Name = "4",
+                    Answers = "asdasdasdasdasdasdasdasdasd"
+                },
+            };
+
+
+            foreach(User user in users)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    if (user.Answers[i] == answers[i])
+                    {
+                        user.Result[i] = true;
+                    }
+                }
+
+                if(user.Answers[1] != answers[1] || user.Answers[5] != answers[5])
+                {
+                    user.Result[1] = false;
+                    user.Result[5] = false;
+                }
+
+                //Обработать разбаловку
+                if (user.Result[1] == true) { user.Grade++; }
+                if (user.Result[5] == true) { user.Grade+=3; }
+                
+                //Добавить обработку под оценку
+                Console.WriteLine("{0} - {1}", user.Name, (user.Grade/35.0)*100 );
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Answers { get; set; }
+        public bool[] Result { get; set; }
+        public double Grade { get; set; }
+
+        public User()
+        {
+            Result = new bool[25];
+            Grade = 0;
+            for(int i = 0; i<25; i++)
+            {
+                Result[i] = false;
+            }
+        }
+    }
+}
